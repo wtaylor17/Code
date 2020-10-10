c55f5bc0aa20f54b5003d8e0471a7c7aa8d126d1
William Taylor-Melanson <wtaylor@upei.ca>
2020-10-10 12:06:30 -0300

    Update union_find.cpp
    Stops union if they are already in the same set

--- snippets/graph/union_find.cpp
+++ snippets/graph/union_find.cpp
@@ -5,6 +5,7 @@ int uf_find(subset* s, int i) {
 
 void uf_union(subset* s, int x, int y) {
 	int xp = uf_find(s, x), yp = uf_find(s, y);
+	if(xp == yp) return;
 	if (s[xp].rank > s[yp].rank) s[yp].p = xp, s[xp].sz += s[yp].sz;
 	else if (s[xp].rank < s[yp].rank) s[xp].p = yp, s[yp].sz += s[xp].sz;
 	else s[yp].p = xp, s[xp].rank++, s[xp].sz += s[yp].sz;
